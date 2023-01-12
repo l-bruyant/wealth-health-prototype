ba3df18c1d2242be4fffd621811dabe2c1592e0c
Lbrt64 <lbruyant@outlook.fr>
2023-01-12 15:24:29 +0100

    Pagination added except select how many results per page

--- src/components/employeesTable/employeesTable.jsx
+++ src/components/employeesTable/employeesTable.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/jsx-key */
 import React, { useEffect, useState, useMemo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import TableHeader from './tableHeader/tableHeader'
+import Pagination from './pagination/pagination'
 
 export default function EmployeesTable () {
 
     const [employees, setEmployees ] = useState([])
+    const [totalItems, setTotalItems] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
+    const ITEMS_PER_PAGE = 10
+    const firstItemIndexDisplayed = (currentPage - 1) * ITEMS_PER_PAGE
+    const lastItemIndexDisplayed = (currentPage - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+
     const initialEmployeesList = useSelector(state => state.employeesList.value)
 
     function createEmployeeKey (employee) {
@@ -27,32 +34,48 @@ export default function EmployeesTable () {
 
     useEffect( () => {
         setEmployees(initialEmployeesList)
-        console.log(employees)
-    })
+    } )
 
     const employeesData = useMemo ( () => {
         let computedEmployeesData = employees
         console.log(computedEmployeesData)
-        return computedEmployeesData
-    }, [employees])
+        setTotalItems(computedEmployeesData.length)
+        let slicedData = computedEmployeesData.slice(
+            firstItemIndexDisplayed,
+            lastItemIndexDisplayed
+        )
+        return slicedData
+    }, [employees, currentPage])
 
     return (
-        <table className="employees-table">
-            <TableHeader headers={headers} />
-            <tbody>
-                {employeesData.map ( employee => (<tr key={createEmployeeKey(employee)}>
-                    <td> {employee.firstName} </td>
-                    <td> {employee.lastName} </td>
-                    <td> {employee.startDate} </td>
-                    <td> {employee.department} </td>
-                    <td> {employee.dateOfBirth} </td>
-                    <td> {employee.street} </td>
-                    <td> {employee.city} </td>
-                    <td> {employee.state} </td>
-                    <td> {employee.zipCode} </td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
+        <>
+            <table className="employees-table">
+                <TableHeader headers={headers} />
+                <tbody>
+                    {employeesData.map ( employee => (<tr key={createEmployeeKey(employee)}>
+                        <td> {employee.firstName} </td>
+                        <td> {employee.lastName} </td>
+                        <td> {employee.startDate} </td>
+                        <td> {employee.department} </td>
+                        <td> {employee.dateOfBirth} </td>
+                        <td> {employee.street} </td>
+                        <td> {employee.city} </td>
+                        <td> {employee.state} </td>
+                        <td> {employee.zipCode} </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div>
+                <Pagination 
+                    total={totalItems}
+                    itemsPerPage = {ITEMS_PER_PAGE}
+                    currentPage = {currentPage}
+                    onPageChange = {page => setCurrentPage(page)}
+                    firstItemIndex = {firstItemIndexDisplayed + 1}
+                    lastItemIndex = {(firstItemIndexDisplayed) + (employeesData.length)}
+                />
+            </div>
+        </>
     )
 }
---
+++ src/components/employeesTable/pagination/pagination.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { useEffect, useState, useMemo } from 'react'
+
+export default function Pagination ({ firstItemIndex, lastItemIndex, total=0, itemsPerPage=10, currentPage=1, onPageChange}) {
+
+    const [totalPages, setTotalPages] = useState(0)
+
+    useEffect( () => {
+        if (total > 0 && itemsPerPage > 0)
+            setTotalPages(Math.ceil(total/itemsPerPage))
+    }, [total, itemsPerPage])
+
+    const paginationItems = useMemo( () => {
+        const pages = []
+        for(let i=1; i <= totalPages; i++) {
+            pages.push(<div className={(i==currentPage? 'pagination-indexNumber pagination-active':'pagination-indexNumber')} key={i}  onClick={() => onPageChange(i)}>
+                {i}
+            </div>)
+        }
+        return pages
+    }, [totalPages, currentPage])
+
+    if (totalPages === 0) return null;
+
+    return (
+        <div className='pagination-container'>
+            <div className='pagination-index'>
+                Showing {firstItemIndex} to {lastItemIndex} of {total} entries
+            </div>
+            <div className='pagination-nav'>
+                <button 
+                    onClick={ () => onPageChange (currentPage - 1)} 
+                    disabled={currentPage === 1}
+                >Previous</button>
+                    {paginationItems}
+                <button 
+                    onClick={ () => onPageChange (currentPage + 1)} 
+                    disabled={currentPage === totalPages}
+                >Next</button>
+            </div>
+        </div>
+    )
+}
