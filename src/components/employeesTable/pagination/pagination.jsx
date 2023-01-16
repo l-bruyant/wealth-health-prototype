8d6223d1caf36ce946c48f7c5e427774fe88f436
Lbrt64 <lbruyant@outlook.fr>
2023-01-16 18:16:23 +0100

    Improved pagination index UX

--- src/components/employeesTable/pagination/pagination.jsx
+++ src/components/employeesTable/pagination/pagination.jsx
@@ -7,17 +7,79 @@ export default function Pagination ({ firstItemIndex, lastItemIndex, total=0, it
     const [totalPages, setTotalPages] = useState(0)
 
     useEffect( () => {
-        if (total > 0 && itemsPerPage > 0)
+        if (total > 0 && itemsPerPage > 0) {
             setTotalPages(Math.ceil(total/itemsPerPage))
+        }
+        if (total == 0) {
+            setTotalPages(1)
+        }
     }, [total, itemsPerPage])
 
     const paginationItems = useMemo( () => {
         const pages = []
-        for(let i=1; i <= totalPages; i++) {
-            pages.push(<div className={(i==currentPage? 'pagination-indexNumber pagination-active':'pagination-indexNumber')} key={i}  onClick={() => onPageChange(i)}>
-                {i}
-            </div>)
-        }
+        if (totalPages === 1) {
+            return ''
+        } else if (totalPages < 5) {
+            for(let i=1; i <= totalPages; i++) {
+                pages.push(
+                <div className={(i==currentPage? 'pagination-indexNumber pagination-active':'pagination-indexNumber')} key={i}  onClick={() => onPageChange(i)}>
+                    {i}
+                </div>)
+            }
+        } else if (currentPage < 4) {
+            for(let i=1; i <= 4; i++) {
+                pages.push(
+                <div className={(i==currentPage? 'pagination-indexNumber pagination-active':'pagination-indexNumber')} key={i}  onClick={() => onPageChange(i)}>
+                    {i}
+                </div>)
+            }
+            pages.push(
+                <div className='pagination-indexNumber pagination-dots' key='...'>
+                    ...
+                </div>)
+            pages.push(
+                <div className='pagination-indexNumber' key={totalPages}  onClick={() => onPageChange(totalPages)}>
+                    {totalPages}
+                </div>)
+        } else if (currentPage > (totalPages - 3)) {
+            pages.push(
+                <div className='pagination-indexNumber' key={1}  onClick={() => onPageChange(1)}>
+                    {1}
+                </div>)
+            pages.push(
+                <div className='pagination-indexNumber pagination-dots' key='...'>
+                    ...
+                </div>)
+            for(let i=(totalPages - 3); i <= totalPages; i++) {
+                pages.push(
+                <div className={(i==currentPage? 'pagination-indexNumber pagination-active':'pagination-indexNumber')} key={i}  onClick={() => onPageChange(i)}>
+                    {i}
+                </div>)
+            }
+        } else if (totalPages > 1) {
+            pages.push( 
+                <div className='pagination-indexNumber' key={1}  onClick={() => onPageChange(1)}>
+                    {1}
+                </div>)
+            pages.push(
+                <div className='pagination-indexNumber pagination-dots' key='...1'>
+                    ...
+                </div>)
+            for(let i=(currentPage - 1); i <= (currentPage + 1); i++) {
+                pages.push(
+                <div className={(i==currentPage? 'pagination-indexNumber pagination-active':'pagination-indexNumber')} key={i}  onClick={() => onPageChange(i)}>
+                    {i}
+                </div>)
+            }
+            pages.push(
+                <div className='pagination-indexNumber pagination-dots' key='...2'>
+                    ...
+                </div>)
+            pages.push(
+                <div className='pagination-indexNumber' key={totalPages}  onClick={() => onPageChange(totalPages)}>
+                    {totalPages}
+                </div>)
+        } 
         return pages
     }, [totalPages, currentPage])
 
@@ -25,9 +87,16 @@ export default function Pagination ({ firstItemIndex, lastItemIndex, total=0, it
 
     return (
         <div className='pagination-container'>
-            <div className='pagination-index'>
-                Showing {firstItemIndex} to {lastItemIndex} of {total} entries
-            </div>
+            { total > 0
+            ?
+                <div className='pagination-index'>
+                    Showing {firstItemIndex} to {lastItemIndex} of {total} entries
+                </div>
+            :
+                <div className='pagination-index pagination-index-error'>
+                    No results available
+                </div>            
+            }
             <div className='pagination-nav'>
                 <button 
                     onClick={ () => onPageChange (currentPage - 1)} 
