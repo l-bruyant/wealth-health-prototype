e13b210ad1aad808de63098b9dfdc6b8d7b0aa83
Lbrt64 <lbruyant@outlook.fr>
2023-01-18 09:01:21 +0100

    Prettier formatting on more files

--- src/components/features/customTable/utils/searchTable.js
+++ src/components/features/customTable/utils/searchTable.js
@@ -1,8 +1,9 @@
-export default function searchTable (table, input) { 
-    let data = table
-    data = data.filter(item =>
-        Object.keys(item).some(key => 
-            item[key].toLowerCase().includes(input.toLowerCase()))
-    )
-    return data
+export default function searchTable(table, input) {
+    let data = table;
+    data = data.filter((item) =>
+        Object.keys(item).some((key) =>
+            item[key].toLowerCase().includes(input.toLowerCase())
+        )
+    );
+    return data;
 }
--- src/components/features/customTable/utils/sortTable.js
+++ src/components/features/customTable/utils/sortTable.js
@@ -1,8 +1,8 @@
-export default function sortTable (table, sortOrder, sortField) { 
-    let data = table
-    const reversed = sortOrder === "asc" ? 1 : -1
+export default function sortTable(table, sortOrder, sortField) {
+    let data = table;
+    const reversed = sortOrder === "asc" ? 1 : -1;
     data = data.sort(
-        (a,b) => 
-            reversed * a[sortField].localeCompare(b[sortField])) 
-    return data
+        (a, b) => reversed * a[sortField].localeCompare(b[sortField])
+    );
+    return data;
 }
